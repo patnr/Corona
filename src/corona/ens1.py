fb4ef85a70772f467b806cb4b68c4c603ed1e682
patricknraanes <patrick.n.raanes@gmail.com>
2020-04-01 19:05:33 +0200

    Init ens

---
+++ src/corona/ens1.py
@@ -0,0 +1,42 @@
+##
+"""
+"""
+
+## Imports
+from corona.utils import *
+from corona.maths import *
+from corona.model import *
+from corona.plotting import *
+
+## Params
+model = SEIR2()
+# Population size
+nPop = 5*10**6
+# Ens size
+N = 20
+# Time -- unit: days
+t_end = 365
+dt    = 1
+tt    = linspace(0 , t_end , int(t_end/dt)+1)
+date0 = datetime(2020,2,12)
+
+## Init ensemble
+x0 = model.init_state(Infected=1/nPop)
+E0 = x0 + zeros((N,len(x0)))
+E0[:,model.iVar("Infected")] = .1/nPop * randn(N)
+E0 = E0.clip(min=1e-9)
+
+## Integrate
+EE = nPop*integrate(model.dxdt, E0, tt)
+
+# Unpack
+state = model.NamedVars(*EE.T)
+
+## Plot
+fig, ax = freshfig(1)
+
+cPlot = Lines(ax,state,tt, date0)
+cPlot.add("Exposed")
+cPlot.add("Infected")
+cPlot.finalize()
+
